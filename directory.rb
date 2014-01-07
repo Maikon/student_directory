d9a0ee302c6d083dce75a3f60a652e067a46af2a
Makis Otman <makisotman@gmail.com>
2014-01-07 15:07:17 +0000

    Added comments explaining puts and print.

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first, we print the list of students
 puts "The students of my cohort at Avengers Academy"
 puts "-----------------"
 puts "Captain America"
@@ -12,6 +13,8 @@ puts "Hawkeye"
 # puts is a method provided by Ruby that takes in this case, a string as an argument.
 # it prints a line of text to the standard output, in this case the terminal.
 
+# then, we print the total count.
 print "Overall, we have "
+# print does not add a newline like puts does, hence why it does not wrap the line.
 print 8
 puts " great soon-to-be Avengers!"
