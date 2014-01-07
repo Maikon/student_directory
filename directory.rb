be91b5f556bbace3d234e27d4598ef8c2590b0a1
Makis Otman <makisotman@gmail.com>
2014-01-07 15:03:01 +0000

    Added student count.

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-puts "The students of my cohort at Makers Academy"
+puts "The students of my cohort at Avengers Academy"
 puts "-----------------"
 puts "Captain America"
 puts "Thor"
@@ -11,3 +11,7 @@ puts "Hawkeye"
 
 # puts is a method provided by Ruby that takes in this case, a string as an argument.
 # it prints a line of text to the standard output, in this case the terminal.
+
+print "Overall, we have "
+print 8
+puts " great soon-to-be Avengers!"
