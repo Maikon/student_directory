e620c1972e446626dcf000b515910c7e7659686b
Makis Otman <makisotman@gmail.com>
2014-01-07 14:59:12 +0000

    Added the students list and fixed README

---
+++ directory.rb
@@ -0,0 +1,13 @@
+puts "The students of my cohort at Makers Academy"
+puts "-----------------"
+puts "Captain America"
+puts "Thor"
+puts "Iron Man"
+puts "Hulk"
+puts "Black Widow"
+puts "Ant-Man"
+puts "Black Panther"
+puts "Hawkeye"
+
+# puts is a method provided by Ruby that takes in this case, a string as an argument.
+# it prints a line of text to the standard output, in this case the terminal.
