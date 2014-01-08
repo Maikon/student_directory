38434938d9e17506b241ac9e62433e4dc5e0cdc1
Makis Otman <makisotman@gmail.com>
2014-01-08 16:15:11 +0000

    User can now choose which names to print by choosing the initial

--- directory.rb
+++ directory.rb
@@ -1,13 +1,16 @@
 
+
+
 def get_students(students)
 	# get the first name
 	puts "First name?"
 	name = gets.chomp
-	return if name.empty?
+	return if name.empty? # goes out if name is empty
 	puts "Last name?"
 	last = gets.chomp
-	# ask user for more info
-	students << { name: name, cohort: :january}
+	
+	# adds 
+	students << { name: name, last_name: last, cohort: :january}
 	if students.count > 1
 		puts students.count.to_s + " Students in total"
 	else
@@ -23,7 +26,7 @@ def input_students
 	students = get_students(entered_students)
 	puts "You want to add more?(y/n)"
 	answer = gets.chomp
-	until answer.empty? || answer.chars.first != "y"
+	until answer.chars.first != "y"
 		get_students(students) 
 		puts "You want to add more?(y/n)"
 		answer = gets.chomp
@@ -32,31 +35,44 @@ def input_students
 end
 
 def print_header
-	puts "The students of my cohort at Avengers Academy"
+	puts "The students of my cohort at Makers Academy"
 	puts "-----------------"
 end
 
 
 
 def print(names)
+	puts "Would you like to print certain names only? Enter the initial if you do so:"
+	initial = gets.chomp
 	names.each.with_index(1) do |student, i|
-		if student[:name].start_with?("A", "a")
-			puts i.to_s + ") #{student[:name]} (#{student[:cohort]} cohort)"
+		if student[:name].downcase.start_with?(initial.downcase)
+			puts i.to_s + ") #{student[:name]} #{student[:last_name]} (#{student[:cohort]} cohort)"
 		end
 	end
 end
 
+# def print(names)
+# 	puts "Would you like to print certain names only? Enter the initial if you do so:"
+# 	initial = gets.chomp
+# 	names.each.with_index(1) do |student, i|
+
+# 		puts i.to_s + ") #{student[:name]} #{student[:last_name]} (#{student[:cohort]} cohort)"
+# 	end
+# end
+
+
+
 
 def print_footer(names)
 	if names.count > 1
-		puts "Overall, we have #{names.length} soon-to-be Avengers!"
+		puts "Overall, we have #{names.length} soon-to-be Super-Coders!"
 	else
-		puts "Overall, we have #{names.length} soon-to-be Avenger!"
+		puts "Overall, we have #{names.length} soon-to-be Super-Coder!"
 	end
 end
 
 
 students = input_students
-print_header # puts the contents of the print_header method
 print(students) # puts each element of the given argument, in this case the names of the students array
+print_header # puts the contents of the print_header method
 print_footer(students) # prints the content in the print_footer method using the given argument, in this case the students.length
