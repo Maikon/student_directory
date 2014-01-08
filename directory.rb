08e07ee760100db66171527e6f3b41a8b900ee92
Makis Otman <makisotman@gmail.com>
2014-01-08 16:27:54 +0000

    Removed header and added it after user chooses initial for names

--- directory.rb
+++ directory.rb
@@ -1,6 +1,3 @@
-
-
-
 def get_students(students)
 	# get the first name
 	puts "First name?"
@@ -34,16 +31,13 @@ def input_students
 	students
 end
 
-def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-----------------"
-end
-
 
 
 def print(names)
 	puts "Would you like to print certain names only? Enter the initial if you do so:"
 	initial = gets.chomp
+	puts "The students of my cohort at Makers Academy"
+	puts "-----------------"
 	names.each.with_index(1) do |student, i|
 		if student[:name].downcase.start_with?(initial.downcase)
 			puts i.to_s + ") #{student[:name]} #{student[:last_name]} (#{student[:cohort]} cohort)"
@@ -51,22 +45,13 @@ def print(names)
 	end
 end
 
-# def print(names)
-# 	puts "Would you like to print certain names only? Enter the initial if you do so:"
-# 	initial = gets.chomp
-# 	names.each.with_index(1) do |student, i|
-
-# 		puts i.to_s + ") #{student[:name]} #{student[:last_name]} (#{student[:cohort]} cohort)"
-# 	end
-# end
-
-
-
 
 def print_footer(names)
 	if names.count > 1
+		puts "-----------------"
 		puts "Overall, we have #{names.length} soon-to-be Super-Coders!"
 	else
+		puts "-----------------"
 		puts "Overall, we have #{names.length} soon-to-be Super-Coder!"
 	end
 end
@@ -74,5 +59,4 @@ end
 
 students = input_students
 print(students) # puts each element of the given argument, in this case the names of the students array
-print_header # puts the contents of the print_header method
 print_footer(students) # prints the content in the print_footer method using the given argument, in this case the students.length
