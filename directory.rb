1a8bed0def4b30f72eb737a59323b42bd7118d9f
Makis Otman <makisotman@gmail.com>
2014-01-13 06:13:30 +0000

    added the plain code from the student_directory

--- directory.rb
+++ directory.rb
@@ -1,11 +1,34 @@
 # Array that holds students
-students = [
-	"Captain America",
-	"Thor Thunder",
-	"Hulk Smash",
-	"Black Panther",
-	"Iron Man"
-]
+# list = [
+# 	{:name => "Captain America", :cohort => :january},
+# 	{:name => "Thor Thunder", :cohort => :january},
+# 	{:name => "Hulk Smash", :cohort => :january},
+# 	{:name => "Black Panther", :cohort => :january},
+# 	{:name => "Iron Man", :cohort => :january}
+# ]
+
+
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, hit return twice"
+	# create empty array
+	students = []
+	# get first name
+	name = gets.chomp
+	# get cohort
+	cohort = gets.chomp
+	# while the name is not empty, repeat this code
+	until name.empty? do # I found until easier to understand and read as a sentence (until name is empty => repeat this code)
+		# add the student hash to the array
+		students << {:name => name, :cohort => cohort} # this adds a hash for each student in the array students
+		puts "Now we have #{students.length} students"
+		# get another name and cohort from the user
+		name = gets.chomp
+		cohort = gets.chomp
+	end
+	# return the array of students
+	students
+end
 
 
 def print_header
@@ -13,21 +36,21 @@ def print_header
 	puts "--------------"
 end
 
-def print(names)
-	names.each do |name|
-		puts name
+def print(students)
+	students.each do |student| # this goes in and grabs the indivual elements, in this case each nested array
+		puts "#{student[:name]} is on the (#{student[:cohort]} cohort)" # from each nested array it gets the first element, index[0] and the second, index[1]
 	end
 end
 
-def print_footer(names)
+def print_footer(students)
 	puts "--------------"
-	puts "Overall, we have #{names.length} great students."
+	puts "Overall, we have #{students.length} great students."
 end
 
 
 
 # IMPORTANT TO REMEMBER –> separate data from operation on the data
-
+list = input_students
 print_header
-print(students)
-print_footer(students)
+print(list)
+print_footer(list)
