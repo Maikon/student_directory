6e472b4ae831e5d69e407e7210d8064564be93db
Makis Otman <makisotman@gmail.com>
2014-01-14 20:57:56 +0000

    started fresh from scratch

--- directory.rb
+++ directory.rb
@@ -1,58 +1,60 @@
+@prompt = "> "
+@spacing = "==============="
 
-def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, hit return twice"
-	# create empty array
-	students = []
-	# get first name
-	name = gets.chomp
-	# get cohort
-	cohort = gets.chomp
-	# while the name is not empty, repeat this code
-	until name.empty? do # I found until easier to understand and read as a sentence (until name is empty => repeat this code)
-		# add the student hash to the array
-		students << {:name => name, :cohort => cohort} # this adds a hash for each student in the array students
-		puts "Now we have #{students.length} students"
-		# get another name and cohort from the user
-		name = gets.chomp
-		cohort = gets.chomp
+# good practise: methods ending in ? should always return a boolean value (true || false)
+# separate data from actions on the data
+
+def greeting
+	puts "Hello there!"
+	puts "Please add the students to the list:"
+end
+
+def submit_students
+	greeting
+
+	puts "Name of student?"
+	@prompt; name = gets.chomp.capitalize!
+	
+	while name.nil?
+		puts "Name of student?"
+		@prompt; name = gets.chomp.capitalize!
 	end
-	# return the array of students
-	students
+
+	puts "And which cohort?"
+	@prompt; cohort = gets.chomp.capitalize!
+
+	while cohort.nil?
+		puts "And which cohort?"
+		@prompt; cohort = gets.chomp.capitalize!
+	end
+
+	array_of_students = []
+
+	array_of_students << { name: name, cohort: cohort }
+
+	array_of_students
+end
+
+def asking_details
+
+end
+
+def store_details?
+	
+end
+
+def saving_details
+	
+end
+
+def add_more_details?
+	
+end
+
+def sort_by_criteria
+	
 end
 
 
-def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "--------------"
-end
 
-# def filter_name(students)
-# 	puts "Would you like to filter names? Type an initial if so:"
-# 	filter_name = gets.chomp
-# 	if filter_name == students[:name]
-# 		puts "Well done."
-# 	end
-# end
-
-def print(students)
-
-	students.each.with_index(1) { |student, index| # this goes in and grabs the indivual elements, in this case each nested array
-		puts index.to_s + ") #{student[:name]} is on the (#{student[:cohort]} cohort)" # from each nested array it gets the first element, index[0] and the second, index[1]
-	}
-end
-
-def print_footer(students)
-	puts "--------------"
-	puts "Overall, we have #{students.length} great students."
-end
-
-
-
-# IMPORTANT TO REMEMBER –> separate data from operation on the data
-list = input_students
-#puts list
-#filter_name(list)
-print_header
-print(list)
-print_footer(list)
+submit_students
