0e361bdc178f1fabc607c8e9819053d4dfa58206
Makis Otman <makisotman@gmail.com>
2014-03-16 17:14:20 +0000

    fixed indentation

--- directory.rb
+++ directory.rb
@@ -1,177 +1,160 @@
 require 'date'
 
-@prompt = "> "
-@spacing = "==============="
+@prompt = '> '
+@spacing = '==============='
 @array_of_students = []
 
 def print_header
-	puts "Hello there!"
-	puts @spacing	
-	puts "We have the following options for you:"
-	puts @spacing
+  puts 'Hello there!'
+  puts @spacing
+  puts 'We have the following options for you:'
+  puts @spacing
 end
-
 
 def interactive_menu
-	puts @spacing
-	puts "1. Input the students."
-	puts "2. Show the students."
-	puts "9. Exit."
-	puts @spacing
-	puts "Please choose one:"
+  puts @spacing
+  puts '1. Input the students.'
+  puts '2. Show the students.'
+  puts '9. Exit.'
+  puts @spacing
+  puts 'Please choose one:'
 
-	print @prompt; answer = gets.chomp.to_i
-	
-	while answer == 0
-		puts "Please choose from 1, 2 and 9."
-	print @prompt; answer = gets.chomp.to_i
-	puts @spacing
-	end
+  print @prompt; answer = gets.chomp.to_i
 
-	case answer
-	when 1
-		input_students
-	when 2
-		show_students(@array_of_students)
-	when 9
-		puts "Adios amigo!"
-		# exit
-	end
-	
+  while answer == 0
+    puts 'Please choose from 1, 2 and 9.'
+    print @prompt; answer = gets.chomp.to_i
+    puts @spacing
+  end
+
+  case answer
+  when 1
+    input_students
+  when 2
+    show_students(@array_of_students)
+  when 9
+    puts 'Adios amigo!'
+  end
 end
-
 
 def input_students
-	puts "Please input students:"
+  puts 'Please input students:'
 
-	puts "Name of student?"
-	@prompt; name = gets.chomp
-	
-	while name.nil?
-		puts "Name of student?"
-		@prompt; name = gets.chomp
-	end
+  puts 'Name of student?'
+  print @prompt; name = gets.chomp
 
-	puts "And which cohort? Provide numerical form (i.e 1 = Jan)"
-	@prompt; cohort = gets.chomp.to_i
+  while name.nil?
+    puts 'Name of student?'
+    print @prompt; name = gets.chomp
+  end
 
-	while cohort.zero?
-		puts "Please provide numerical form (i.e 1 = Jan)"
-		@prompt; cohort = gets.chomp.to_i
-	end
+  puts 'And which cohort? Provide numerical form (i.e 1 = Jan)'
+  print @prompt; cohort = gets.chomp.to_i
 
-	current_details = { name: name, cohort: Date::ABBR_MONTHNAMES[cohort] }
-	
-	@array_of_students << current_details if store_details?(current_details) == true
-		
+  while cohort.zero?
+    puts 'Please provide numerical form (i.e 1 = Jan)'
+    print @prompt; cohort = gets.chomp.to_i
+  end
 
-	input_students if add_more_details? == true
+  current_details = { name: name, cohort: Date::ABBR_MONTHNAMES[cohort] }
 
-	@array_of_students
+  @array_of_students << current_details if store_details?(current_details) == true
 
-	see_menu
+  input_students if add_more_details? == true
+
+  @array_of_students
+
+  see_menu
 end
-
 
 def yes_or_no?
-	print @prompt; answer = gets.chomp.capitalize!
-	while  answer.nil? || !answer.match(/[YN]/)
-		puts "Please provide an answer (Yes/No):"
-		print @prompt; answer = gets.chomp.capitalize!
-	end
-	answer.start_with?("No", "N") ? false : true
+  print @prompt; answer = gets.chomp.capitalize!
+  while  answer.nil? || !answer.match(/[YN]/)
+    puts 'Please provide an answer (Yes/No):'
+    print @prompt; answer = gets.chomp.capitalize!
+  end
+  answer.start_with?('No', "N") ? false : true
 end
-
 
 def store_details?(details)
-	puts "Are the following details correct? (Yes/No)"
-	puts @spacing
-	puts "Name: #{details[:name]}, Cohort: #{details[:cohort]}?"
-	puts @spacing
-	yes_or_no?
+  puts 'Are the following details correct? (Yes/No)'
+  puts @spacing
+  puts "Name: #{details[:name]}, Cohort: #{details[:cohort]}?"
+  puts @spacing
+  yes_or_no?
 end
 
-
 def add_more_details?
-	puts "Would you like to add more details? (Yes/No)"
-	yes_or_no?
+  puts 'Would you like to add more details? (Yes/No)'
+  yes_or_no?
 end
 
 def sort_list
-	puts "Would you like to sort the list?"
-	if yes_or_no? == true
-		puts "Sort by cohort or name?"
-		print @prompt; answer = gets.chomp
-					   
+  puts 'Would you like to sort the list? (Yes/No)'
+  if yes_or_no? == true
+    puts 'Sort by cohort or name?'
+    print @prompt; answer = gets.chomp
+    while answer.nil? || !answer.match(/[CcNn]/)
+      puts 'Please choose between Cohort and Name:'
+      print @prompt; answer = gets.chomp
+    end
 
-		while answer.nil? || !answer.match(/[CcNn]/)
-			puts "Please choose between Cohort and Name:"
-			print @prompt; answer = gets.chomp				   
-		end
-
-		if answer.match(/[Cc]/)
-			sort_by_cohort
-		else
-			sort_by_name
-		end
-	else
-		
-	end
-
+    if answer.match(/[Cc]/)
+      sort_by_cohort
+    else
+      sort_by_name
+    end
+  else
+  end
 end
 
 def sort_by_cohort(list)
-	puts "Choose a cohort you would like to see:"
+  puts 'Choose a cohort you would like to see:'
 end
 
 def sort_by_name
-	puts "Choose an initial for names you wish to see:"
+  puts 'Choose an initial for names you wish to see:'
 end
 
 def empty_student_list
-	puts "You haven't added anything I'm afraid..."
-	puts "Would you like to add some?(Yes/No)"
-	if yes_or_no? == true
-		input_students 
-	else
-		see_menu
-	end
+  puts "You haven't added anything I'm afraid..."
+  puts 'Would you like to add some?(Yes/No)'
+  if yes_or_no? == true
+    input_students
+  else
+    see_menu
+  end
 end
-
 
 def show_students(list)
-	if list.empty?
-		empty_student_list
-	else
-		sort_list
-		puts @spacing
-		puts "These are the following students: "
-		list.each.with_index(1) do |student, i| 
-			puts i.to_s + ") #{student[:name]}, (#{student[:cohort]} cohort)"
-		end
-		puts @spacing
-		see_menu
-	end
+  if list.empty?
+    empty_student_list
+  else
+    sort_list
+    puts @spacing
+    puts 'These are the following students: '
+    list.each.with_index(1) do |student, i|
+      puts i.to_s + ") #{student[:name]}, (#{student[:cohort]} cohort)"
+    end
+    puts @spacing
+    see_menu
+  end
 end
-
 
 def print_footer(list)
-	puts @spacing
-	puts "Overall, we have #{list.length} students joining in."
+  puts @spacing
+  puts "Overall, we have #{list.length} students joining in."
 end
-
 
 def see_menu
-	puts "Would you like to see the menu again?"
-	if yes_or_no? == true
-		interactive_menu
-	else
-		puts "Adios Amigo!"
-	end
-	exit
+  puts 'Would you like to see the menu again?(Yes/No)'
+  if yes_or_no? == true
+    interactive_menu
+  else
+    puts 'Adios Amigo!'
+  end
+  exit
 end
-
 
 print_header
 interactive_menu
-
